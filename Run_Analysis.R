e57719b05a1bb9a377f70ae9bf74e41674a79655
MahaDawass <63047653+MahaDawass@users.noreply.github.com>
2020-04-15 19:12:27 +0300

    Update Run_Analysis.R

--- Run_Analysis.R
+++ Run_Analysis.R
@@ -1,11 +1,7 @@
 #### Getting and Cleaning data
 
-### clean the Enviroment Screen
 
-rm(list = ls())
-
-
-### (1) Merges the training and the test sets to create one data set.
+### Request (1) 
 
 if(!dir.exists("Assignment")){dir.create("Assignment")}
 
@@ -21,14 +17,13 @@ y_train <- read.table("Assignment/UCI HAR Dataset/train/y_train.txt")
 train_Subjects <- read.table("Assignment/UCI HAR Dataset/train/subject_train.txt")
 
 
-### Merges test, train, and all data
+### Merge  data
 
 merge_data1 <- cbind(y_test, test_Subjects, X_test)
 merge_data2 <- cbind(y_train,train_Subjects, X_train)
 merge_all_data <- rbind(merge_data1 , merge_data2 )
 
-### (2) Extracts only the measurements on the mean and standard deviation for each measurement. 
-
+### Request (2) 
 features <- read.table("Assignment/UCI HAR Dataset/features.txt")
 features_statictic <- grep(".*mean.*|.*std.*", features[,2])
 features_statictic <- features[features_statictic,2]
@@ -36,17 +31,17 @@ features_statictic = gsub('-mean', 'Mean', features_statictic)
 features_statictic = gsub('-std', 'Std', features_statictic)
 features_statictic <- gsub('[-()]', '', features_statictic)
 
-### (4) Appropriately labels the data set with descriptive variable names. 
+### Request (4) 
 
 colnames(merge_all_data) <- c("activity", "subject",  features_statictic)
 
-### (3) Uses descriptive activity names to name the activities in the data set.
+### Request (3) 
 
 activity <- read.table("Assignment/UCI HAR Dataset/activity_labels.txt")
 merge_all_data$activity <- factor(merge_all_data$activity, levels = activity[,1], labels = activity[,2])
 merge_all_data$subject <- as.factor(merge_all_data$subject)
 
-###  (5) From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+###  Request (5) 
 library(reshape2)
 merge_all_data.melt <- melt(merge_all_data, id = c("activity","subject"))
 merge_all_data.mean <- dcast(merge_all_data.melt, activity+subject ~ variable, mean)
